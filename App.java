b4c95d5759c48d1fab10e308d12ed4fc148bf016
IqbalSobari <1035607@hr.nl>
2024-10-21 14:56:03 +0200

    functies

--- App.java
+++ App.java
@@ -3,6 +3,11 @@ public class App {
     public static void main(String[] args) {
         System.out.println("F1");
         F1_CareerMode player1 = new F1_CareerMode();
+        
+        player1.kiesjouwteam();
+        player1.kiesjouwauto();
+        player1.kiesjouwmotor();
         player1.startRace();
+        player1.verstuurbericht();
     }
 }
--- F1_CareerMode.java
+++ F1_CareerMode.java
@@ -1,32 +1,32 @@
 public class F1_CareerMode {
+    private Team mercedes = null ; 
+    private Team ferrari = null;
+    private Team mclaren = null; 
     
-    Team mercedes = new Mercedes(new Monteur(), new RaceEngineer(), new MCL60(new Ferrari_065()), "Mercedes");
-    private Team ferrari = new Ferrari(new Monteur(), new RaceEngineer(), new W15(new Honda_RA620H()), "Ferrari");
-    private Team mclaren = new Mclaren(new Monteur(), new RaceEngineer(), new SF_24(new Renault_20()), "Mclaren");
+    public void kiesjouwteam() {
+     
+    }
+
+    public void kiesjouwauto() {
+     
+    }
+
+    public void kiesjouwmotor() {
+        
+    }
 
     
-
     
-    
-
 
     public void startRace() {
         
-        System.out.println(ferrari.getTeamname());
-        mclaren.e.taak();
-        
-
-
-
-
-       
-        
-        
-
-        
-
 
     }
+
+    public void verstuurbericht() {
+        
+    }
+
 
         
 
