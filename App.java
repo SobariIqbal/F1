2dc7d0d725dbb8c9114cb5d40067063c299f7610
IqbalSobari <1035607@hr.nl>
2024-11-03 20:13:08 +0100

    namen studenten

--- App.java
+++ App.java
@@ -1,4 +1,8 @@
-
+// design pattern 
+// Soufiane fallou 1058454
+// Yassir balah 1060657
+// iqbal Sobari 1035607
+// George Naranjo Silva 1050296
 public class App {
 
     public static void main(String[] args) {
